8aa5fcea45d5b26d9112941066907213bd4280dd
Philipp Riegger <philipp.riegger@itemis.com>
2018-02-16 15:19:49 +0100

    Add and use lodash

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash"
+
 interface IStartup {
     main(): number
 }
@@ -5,7 +7,7 @@ interface IStartup {
 class Startup implements IStartup {
     public main(): number {
         // tslint:disable-next-line:no-console
-        console.log("Hello World")
+        console.log(_.padStart("Hello World", 20, " "))
         return 0
     }
 }
